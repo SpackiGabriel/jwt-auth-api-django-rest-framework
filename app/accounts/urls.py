98410474616326c357598282e17680ea29853b5f
Gabriel Spacki <ifprgabriel@gmail.com>
2024-06-18 00:33:21 -0300

    :sparkles: feat: added basic auth urls

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,7 +1,17 @@
+from dj_rest_auth.jwt_auth import get_refresh_view
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
+
 from django.urls import path
-from . import views
+
+from rest_framework_simplejwt.views import TokenVerifyView
 
 
 urlpatterns = [
-
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("user/", UserDetailsView.as_view(), name="user-details"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
+    path("token/refresh/", get_refresh_view().as_view(), name="token-refresh"),
 ]
